53201b363528aee8b500ff276b1f7128d0fdb7f5
BaptisteCMY <baptiste.comoy2501@gmail.com>
2023-10-23 14:04:11 +0200

    test

---
+++ assets/memory.sql
@@ -0,0 +1,45 @@
+
+CREATE DATABASE power_of_memory CHARACTER SET utf8;
+USE power_of_memory ;
+CREATE TABLE player(
+    id_player INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(40) NOT NULL,
+    player_password VARCHAR(256) NOT NULL,
+    pseudo VARCHAR(20) NOT NULL,
+    date_sign_up DATETIME NOT NULL,
+    date_last_login DATETIME NOT NULL,
+    PRIMARY KEY(id_player)
+    );
+
+    
+
+CREATE TABLE scores(
+    id_score INT NOT NULL AUTO_INCREMENT,
+    id_player INT NOT NULL,
+    id_game INT NOT NULL,
+    game_strength INT NOT NULL,
+    game_score INT NOT NULL,
+    game_date DATETIME NOT NULL,
+    PRIMARY KEY(id_score)
+    );
+
+CREATE TABLE message(
+    id_message INT NOT NULL AUTO_INCREMENT,
+    id_game INT NOT NULL,
+    id_sender INT NOT NULL,
+    comment TEXT NOT NULL,
+    date_comment DATETIME NOT NULL,
+    PRIMARY KEY(id_message)
+    );
+
+CREATE TABLE games(
+    id_game INT NOT NULL AUTO_INCREMENT,
+    game_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY(id_game)
+    );
+
+
+
+UPDATE players
+SET player_password = "v "
+
