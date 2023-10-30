32bd5a8ec47ce75dbcf475c7dcf3eeb281ba8598
Bataillon Junior <jdd78800@gmail.com>
2023-10-30 13:58:39 +0100

    ConnexionDbEtTestHeaderFooter

--- login.php
+++ login.php
@@ -1,14 +1,9 @@
+
+<?php require_once("utils/common.php") ?>
+
 <!DOCTYPE html>
   <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="assets/css/header.css">
-        <link rel="stylesheet" href="assets/css/main_score.css">
-        <link rel="stylesheet" href="assets/css/footerindex.css">
-        <script src="https://kit.fontawesome.com/fd7b39a087.js" crossorigin="anonymous"></script>
-    </head>
+   <?= require_once(SITE_ROOT."partials/head.php");?>
     <body style="background-color: #151231; background-size: 1520px 800px;"></body>
 
     <body>
@@ -60,42 +55,7 @@
           </section>
 
 
-          <footer style="background-color: #0c0b26;">
-            <div class="footer-content">
-                <div class="info">
-                    <div class="miniinfo">
-                        <h4 style="font-size: 1.4vw; color: white">Informations</h4>
-                        <p class="mot_gris">Quisque commodo facilisis purus,</p>
-                        <p class="mot_gris"><span class="mot_orange">Tel :</span> 06 66 66 66 66</p>
-                        <p class="mot_gris"><span class="mot_orange">Email :</span> support@powerofmemory.com</p>
-                        <p class="mot_gris"><span class="mot_orange">Location :</span> Paris</p>
-                        <div class="social-media">
-                            <button class="logo_marque"><i class="fa-brands fa-facebook"></i></button>
-                            <button class="logo_marque"><i class="fa-brands fa-twitter"></i></button>
-                            <button class="logo_marque"><i class="fa-brands fa-google"></i></button>
-                            <button class="logo_marque"><i class="fa-brands fa-pinterest"></i></button>
-                            <button class="logo_marque"><i class="fa-brands fa-instagram"></i></button>
-                        </div>
-                    </div>
-                </div>
-                <div class="pom">
-                    <div class="pomtitre"> <h4 style="font-size: 1.4vw; color: white;">Power of Memory</h4></div>
-                    <div class="listpom">
-                        <ul>
-                            <li style="color: #ec9123; list-style-type: square;"><a href="jeu.html" class="list">Jouer !</a></li>
-                            <li style="color: #ec9123; list-style-type: square;"><a href="score.html" class="list">Les scores</a></li>
-                            <li style="color: #ec9123; list-style-type: square;"><a href="contact.html" class="list">Nous contacter</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <br>
-        <div class="ancre"><a href="#top" style="color: #1e1c38; text-align: center; text-decoration: none; font-size: 3vw; padding: 0 1vw;">^</a></div>
-            <br>
-            <p class="mot_gris" style="margin-left: 13.5vw;">Copyright © 2022 Tous droits réservés</p>
-            <br>
-        </footer>
+          <?= require_once(SITE_ROOT."partials/footer.php"); ?>
     </body>
 
 
--- utils/common.php
+++ utils/common.php
@@ -3,7 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-define('PROJECT_FOLDER', '/projet-flash-jeu-memory/'); 
+define('PROJECT_FOLDER', '/COURSES/powerofmemory/memories/'); 
 define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . PROJECT_FOLDER); 
 
 session_start();
+
+
+?>
+
+
